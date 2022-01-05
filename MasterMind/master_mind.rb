09689eb9544be181549c89fc1827fcb4917286fd
Rian Negreiros <riannegreiros@gmail.com>
2022-01-05 20:29:14 -0300

    using a loop instead of ifs

--- MasterMind/master_mind.rb
+++ MasterMind/master_mind.rb
@@ -1,32 +1,23 @@
-CODE = ['1', '2', '3', '4']
-quess = ''
+CODE = %w[1 2 3 4].freeze
+guess = ''
 attempts = 1
 MAX_ATTEMPTS = 12
 
-while quess != CODE && attempts <= MAX_ATTEMPTS
-  raw_quess = gets.chomp
-  quess = raw_quess.split('')
+while guess != CODE && attempts <= MAX_ATTEMPTS
+  raw_guess = gets.chomp
+  guess = raw_guess.split('')
 
   number_correct = 0
-  if quess[0] == CODE[0]
-    number_correct += 1
-  end
-  if quess[1] == CODE[1]
-    number_correct += 1
-  end
-  if quess[2] == CODE[2]
-    number_correct += 1
-  end
-  if quess[3] == CODE[3]
-    number_correct += 1
+  (0..CODE.length - 1).each do |i|
+    number_correct += 1 if guess[i] == CODE[i]
   end
 
   print "\nYou got #{number_correct} correct.\n"
   attempts += 1
 end
 
-if quess == CODE
-  print "You quessed the code!"
+if guess == CODE
+  print 'You guessed the code!'
 else
-  print "Sorry, you lose"
+  print 'Sorry, you lose'
 end
