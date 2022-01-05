3cc8c190d539501fc467b9be96d953c9bc1057af
Rian Negreiros <riannegreiros@gmail.com>
2022-01-05 20:18:11 -0300

    limiting the number of attempts

--- MasterMind/master_mind.rb
+++ MasterMind/master_mind.rb
@@ -1,10 +1,15 @@
 CODE = '1234'
 quess = ''
 attempts = 1
+MAX_ATTEMPTS = 12
 
-while quess != CODE
+while quess != CODE && attempts <= MAX_ATTEMPTS
   quess = gets.chomp
-  attempts = attempts += 1
+  attempts += 1
 end
 
-print "You quessed the code!"
+if quess == CODE
+  print "You quessed the code!"
+else
+  print "Sorry, you lose"
+end
