4aed04d01b26dd8d06a6b3d2fb72f6ebf4825dbd
Rian Negreiros <riannegreiros@gmail.com>
2022-01-05 20:23:15 -0300

    using a list

--- MasterMind/master_mind.rb
+++ MasterMind/master_mind.rb
@@ -1,10 +1,25 @@
-CODE = '1234'
+CODE = ['1', '2', '3', '4']
 quess = ''
 attempts = 1
 MAX_ATTEMPTS = 12
 
 while quess != CODE && attempts <= MAX_ATTEMPTS
-  quess = gets.chomp
+  raw_quess = gets.chomp
+  quess = raw_quess.split('')
+
+  number_correct = 0
+  if quess[0] == CODE[0]
+    number_correct += 1
+  end
+  if quess[1] == CODE[1]
+    number_correct += 1
+  end
+  if quess[2] == CODE[2]
+    number_correct += 1
+  end
+  if quess[3] == CODE[3]
+    number_correct += 1
+  end
   attempts += 1
 end
 
