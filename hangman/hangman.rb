e8fceabe235a56805dda57e26ee131e96a9b1e1a
Rian Negreiros <riannegreiros@gmail.com>
2022-01-14 18:52:00 -0300

    select random word method

---
+++ hangman/hangman.rb
@@ -0,0 +1,16 @@
+def select_random_word
+  dictionary = File.open("5desk.txt")
+
+  words = Array.new
+  random_word = ''
+
+  dictionary.each do |word|
+    words.append(word)
+  end
+
+  until random_word.length >= 5 && random_word.length <= 12 do
+    random_word = words.sample
+  end
+
+  random_word
+end
