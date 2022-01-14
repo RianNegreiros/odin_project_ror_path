fb19f94527534e0d7a6b465d1844d664d75bda35
Rian Negreiros <riannegreiros@gmail.com>
2022-01-14 19:31:11 -0300

    hangman method

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -16,11 +16,9 @@ def select_random_word
 end
 
 def print_word(values)
-  print("\t")
   values.each { |x|
-    print(x)
+    print("#{x}\n")
   }
-  print("\t")
 end
 
 def check_win(values)
@@ -31,3 +29,63 @@ def check_win(values)
   }
   true
 end
+
+def hangman(word)
+  word_display = []
+
+  correct_letters = []
+
+  incorrect = []
+
+  chances = 0
+
+  limit = 5
+
+  word.each_char { |char|
+    word_display.append('_')
+    correct_letters.append(char.upcase)
+  }
+
+  while true
+    print_word(word_display)
+    print("Incorrect characters: #{incorrect}\n")
+
+    print("Enter a character = ")
+    input = gets.chomp
+
+    if input.length != 1
+      print("Only one letter.")
+      next
+    elsif not input.match? /\A[a-zA-Z'-]*\z/
+      print("Must be a letter.")
+    elsif incorrect.upcase.include? input
+      print("Already tried")
+      next
+    end
+
+    if correct_letters.upcase.include?(input) == false
+      incorrect.append(input.upcase)
+
+      chances += 1
+
+      if chances == limit
+        print("\nGAME OVER!\n")
+        print("The word is: #{word.upcase}")
+        break
+      end
+    else
+      (0..word.length - 1).each do |i|
+        if word[i].upcase == input.upcase
+          word_display[i] = input.upcase
+        end
+      end
+      if check_win(word_display)
+        print("\nCongratulations!\n")
+        print("The word is: #{word.upcase}")
+        break
+      end
+    end
+  end
+end
+
+hangman(select_random_word)
