b6f842f8bd6c6ecccb44134514d4137ada162089
Rian Negreiros <riannegreiros@gmail.com>
2022-03-05 14:57:07 -0300

    append method

---
+++ linked_lists/linked_lists.rb
@@ -0,0 +1,20 @@
+class Node
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+class LinkedList
+  def initialize
+    @head = nil
+  end
+    # adds a new node containing value to the end of the list
+  def append(value)
+    curr_node = @head
+    while curr_node.next != nil
+      curr_node = curr_node.next
+    end
+    curr_node.next = Node.new(value, nil)
+  end
+end
