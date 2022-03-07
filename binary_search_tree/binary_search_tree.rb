b0c2299c79bb94d2a93b2c71dd5f80c3db35094a
Rian Negreiros <riannegreiros@gmail.com>
2022-03-07 09:05:53 -0300

    insert method

--- binary_search_tree/binary_search_tree.rb
+++ binary_search_tree/binary_search_tree.rb
@@ -22,4 +22,26 @@ class Tree
     root.right = build_tree(arr.slice(mid+1, arr.length))
     root
   end
+
+  def insert(value)
+    return Node.new(value) if @root.nil?
+
+    curr_node = @root
+    prev_node = @root
+
+    while !curr_node.nil?
+      prev_node = curr_node
+      if value < curr_node.data
+        curr_node = curr_node.left
+      else
+        curr_node = curr_node.right
+      end
+    end
+
+    if value < prev_node.data
+      prev_node.left = Node.new(value)
+    else
+      prev_node.right = Node.new(value)
+    end
+  end
 end
