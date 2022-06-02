4b2f43696d323dcbd30325dbcff4705206fd81eb
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-06-02 12:14:07 -0300

    create todo

--- rails/fullstack-todolist/app/controllers/todos_controller.rb
+++ rails/fullstack-todolist/app/controllers/todos_controller.rb
@@ -1,4 +1,22 @@
 class TodosController < ApplicationController
 
-  private
+  before_action :set_todo, only: [:show, :update, :destroy]
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render json: @todo
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  private 
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
+
+    def todo_params
+      params.require(:todo).permit(:id, :title, :completed)
+    end
 end
