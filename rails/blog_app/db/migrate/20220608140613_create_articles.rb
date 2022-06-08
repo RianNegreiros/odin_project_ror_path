08a60454a73461fc9f765f990d4300f353218c1b
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-06-08 11:10:22 -0300

    create articles migration

---
+++ rails/blog_app/db/migrate/20220608140613_create_articles.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Migration to create articles table
+class CreateArticles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :description
+      t.timestamps
+    end
+  end
+end
