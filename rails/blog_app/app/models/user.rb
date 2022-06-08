ce17429f8a122994420a1444fabb9bb8cf5efcc9
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-06-08 14:09:06 -0300

    add password digest to user

--- rails/blog_app/app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  has_many :articles
-  before_save { self.email = email.downcase }
-  validates :username, presence: true,
-                       uniqueness: { case_sensitive: false },
-                       length: { minimum: 3, maximum: 25 }
-  validates :email, presence: true, length: { maximum: 105 },
-                    uniqueness: { case_sensitive: false }
-end
