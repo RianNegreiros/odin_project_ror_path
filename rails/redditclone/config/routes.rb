d64a87a7de804c0daa90fe01bd71a951074dbdb7
Rian Negreiros <riannegreiros@gmail.com>
2023-02-14 15:58:16 -0300

    added and set up act_as_votable

--- rails/redditclone/config/routes.rb
+++ rails/redditclone/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :links
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :links do
+    member do
+      put "like", to: "links#upvote"
+      put "dislike", to: "links#downvote"
+    end
+  end
   root to:'links#index'
 end
