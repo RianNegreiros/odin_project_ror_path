71331732b6008d576e53174a6e3809503d77a4da
Rian Negreiros <riannegreiros@gmail.com>
2023-02-19 17:00:14 -0300

    refactor caesar cipher

--- ruby/caesar_cipher/caesar_cipher.rb
+++ ruby/caesar_cipher/caesar_cipher.rb
@@ -1,17 +1,30 @@
-def caesar_cipher(plaintext, factor)
-  codepoints_array = []
-
-  a_codepoint = 'a'.ord
-
-  plaintext.split('').each do |letter|
-    if letter =~ /[^a-zA-Z]/
-      codepoints_array << letter.bytes.join('').to_i
+def caesar_cipher(string, shift)
+  # Create an array of all the letters in the alphabet
+  alphabet = Array('a'..'z')
+  
+  # Create a hash that maps each letter to its corresponding index in the alphabet array
+  letter_index = Hash[alphabet.map.with_index.to_a]
+  
+  # Convert the input string to lowercase and split it into an array of characters
+  string_array = string.downcase.split('')
+  
+  # Iterate over each character in the string
+  new_string = string_array.map do |char|
+    # If the character is not a letter, leave it as is
+    if !alphabet.include?(char)
+      char
     else
-      shifted_codepoint = letter.downcase.bytes.join('').to_i + factor
-      codepoints_array << (shifted_codepoint - a_codepoint) % 26 + a_codepoint
+      # Determine the new index of the letter after shifting by the specified amount
+      new_index = (letter_index[char] + shift) % 26
+      
+      # Map the new index back to its corresponding letter in the alphabet array
+      alphabet[new_index]
     end
   end
-  ciphertext = codepoints_array.pack 'C*'
-  puts ciphertext
+  
+  # Join the array of characters back into a string and return it
+  new_string.join('')
 end
-caesar_cipher("What a string!", 5)
+
+encrypted = caesar_cipher("hello world", 3)
+puts encrypted  # => "khoor zruog"
