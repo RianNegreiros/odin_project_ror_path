5cf13b4c36b082a0ceca85543cd54caa0de99cd3
Rian Negreiros <riannegreiros@gmail.com>
2023-02-20 15:08:15 -0300

    refactor tic tac toe

--- ruby/tic_tac_toe/tic_tac_toe.rb
+++ ruby/tic_tac_toe/tic_tac_toe.rb
@@ -1,120 +1,89 @@
-class Person
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
+class TicTacToe
+  def initialize
+    @board = Array.new(3) { Array.new(3, " ") }
+    @current_player = "X"
   end
-end
-
-class Player < Person
-  attr_reader :sign
-
-  def initialize(name, sign)
-    super(name)
-    @sign = sign
+  
+  def print_board
+    puts "  1 2 3"
+    puts "1 #{@board[0][0]}|#{@board[0][1]}|#{@board[0][2]}"
+    puts "  -+-+-"
+    puts "2 #{@board[1][0]}|#{@board[1][1]}|#{@board[1][2]}"
+    puts "  -+-+-"
+    puts "3 #{@board[2][0]}|#{@board[2][1]}|#{@board[2][2]}"
   end
-
-  def print
-    puts "Name: #{@name}, Sign: #{@sign}"
-  end
-end
-
-class Board
-  def initialize(player1, player2)
-    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-    @player1 = player1
-    @player2 = player2
-  end
-
-  def reset_board
-    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-  end
-
-  def display_board
-    puts "  #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts ' ---+---+---'
-    puts "  #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts ' ---+---+--- '
-    puts "  #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
-
-  def position_taken?(position)
-    @board[position] != ' '
-  end
-
-  def valid_move?(position)
-    position.between?(0, 8) && !position_taken?(position)
-  end
-
-  def turn
-    @current_player = @turn_count.odd? ? @player1 : @player2
-    print "#{@current_player}, Please choose a number between 1-9: "
-    position = gets.chomp.to_i
-    position -= 1
-    if valid_move?(position)
-      @board[position] = @current_player.sign
+  
+  def make_move(row, col)
+    if @board[row][col] == " "
+      @board[row][col] = @current_player
+      switch_player
+      true
     else
-      puts "Invalid position or input"
-      turn
+      false
     end
   end
-
-  def play
-    @turn_count = 0
-    until over?
-      @turn_count += 1
-      turn
-      puts "###########"
-      display_board
+  
+  def game_over?
+    winner? || tie?
+  end
+  
+  def winner?
+    # check rows
+    @board.each do |row|
+      return true if row.uniq.length == 1 && row[0] != " "
     end
-    print "Game Over "
-
-    if won?
-      puts "Congratulations #{@current_player.name} you are the winner."
-    else
-      puts "Its a draw, better luck next time !"
+    
+    # check columns
+    @board.transpose.each do |col|
+      return true if col.uniq.length == 1 && col[0] != " "
     end
-    puts "Doy want to start again? press Y/N"
-    answer = gets.chomp
-    if %w[Y y].include?(answer)
-      reset_board
-      play
-    else
-      puts "See you again !"
-    end
+    
+    # check diagonals
+    return true if [@board[0][0], @board[1][1], @board[2][2]].uniq.length == 1 && @board[0][0] != " "
+    return true if [@board[0][2], @board[1][1], @board[2][0]].uniq.length == 1 && @board[0][2] != " "
+    
+    false
+  end
+  
+  def tie?
+    @board.flatten.none? { |cell| cell == " " }
+  end
+  
+  private
+  
+  def switch_player
+    @current_player = (@current_player == "X") ? "O" : "X"
   end
 end
 
-class TicTacToe < Board
-  WIN_COMBINATIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [6, 4, 2],
-    [0, 4, 8]
-  ].freeze
+game = TicTacToe.new
 
-  def draw?
-    !won? && full?
+puts "Welcome to Tic-Tac-Toe!"
+puts "Player 1: X"
+puts "Player 2: O"
+puts ""
+
+until game.game_over?
+  game.print_board
+  puts ""
+  
+  player = (game.instance_variable_get(:@current_player) == "X") ? 1 : 2
+  puts "Player #{player}, make your move (row column):"
+  
+  move = gets.chomp.split.map(&:to_i)
+  if game.make_move(move[0]-1, move[1]-1)
+    puts ""
+  else
+    puts "Invalid move. Try again."
   end
-
-  def full?
-    @turn_count == 9
-  end
-
-  def won?
-      WIN_COMBINATIONS.detect do |combo|
-        @board[combo[0]] == @board[combo[1]] &&
-          @board[combo[1]] == @board[combo[2]] &&
-          position_taken?(combo[0])
-    end
-  end
-
-    def over?
-      won? || full? || draw?
-    end
 end
 
+game.print_board
+puts ""
+
+if game.winner?
+  player = (game.instance_variable_get(:@current_player) == "X") ? 2 : 1
+  puts "Congratulations, Player #{player}! You win!"
+else
+  puts "It's a tie!"
+end
