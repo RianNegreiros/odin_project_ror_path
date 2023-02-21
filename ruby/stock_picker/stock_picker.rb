238c6ae616dd8b712448e99278bb71a8760f6217
Rian Negreiros <riannegreiros@gmail.com>
2023-02-21 15:06:53 -0300

    refactor stock picker method

--- ruby/stock_picker/stock_picker.rb
+++ ruby/stock_picker/stock_picker.rb
@@ -1,19 +1,23 @@
-def stock_picker(arr)
-  last = 0
-  buy = 0
-  sell = 0
-
-  arr.each_with_index do |day, index|
-    (index + 1..arr.length - 1).each do |j|
-      profit = arr[j] - day
-      next unless profit > last
-
-      last = profit
-      buy = index
-      sell = j
+def stock_picker(prices)
+  max_profit = 0
+  buy_day = 0
+  sell_day = 0
+  
+  prices.each_with_index do |buy_price, day|
+    (day+1..prices.length-1).each do |sell_day_index|
+      sell_price = prices[sell_day_index]
+      if sell_price - buy_price > max_profit
+        max_profit = sell_price - buy_price
+        buy_day = day
+        sell_day = sell_day_index
+      end
     end
   end
-  [buy, sell]
+  
+  [buy_day, sell_day]
 end
 
-p stock_picker([0, 3, 6, 9, 5, 8, 6, 1, 10])
+prices = [17,3,6,9,15,8,6,1,10]
+best_days = stock_picker(prices)
+puts "Best day to buy: #{best_days[0]}"
+puts "Best day to sell: #{best_days[1]}"
